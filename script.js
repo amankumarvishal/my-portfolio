120d4f2c2710e797d7eba37648125a8fafa61d94
Aman Kumar <56917962+amankumarvishal@users.noreply.github.com>
2025-02-17 21:06:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Navigation Menu Toggle
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-links li');
